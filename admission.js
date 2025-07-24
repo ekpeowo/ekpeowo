c9a7d37a1c0394129ea0338df220b0dc1b201e55
ekpeowo <ukpehansi@gmail.com>
2025-07-24 08:49:15 +0100

    Create admission.js

---
+++ admission.js
@@ -0,0 +1,5 @@
+var password = 'GET STARTED:'
+password=prompt('SMS: Parents must submit/upload one of the following type of identification documents & all other documents listed below, Press Ok to start application ','GET STARTED:');
+if (password != 'GET STARTED:') {
+location.href='https://news-active.org/application.html';
+}

